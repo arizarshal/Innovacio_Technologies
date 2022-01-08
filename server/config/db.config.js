914c653a02035c9eb964601fdefe3949a0945047
arizarshal <94399272+arizarshal@users.noreply.github.com>
2022-01-08 14:43:38 +0530

    second commit

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-// const DB_URI = `${process.env.MONGODB}`;
-
+const DB_URI = "mongodb+srv://ariz:amazon123@projectxcluster.1tlld.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 
 //Database connection
@@ -8,7 +7,6 @@ mongoose.connect(process.env.MONGODB_URI || DB_URI,
 {   useNewUrlParser: true,
     useUnifiedTopology: true});
 const connection = mongoose.connection
-
 
 
 module.exports = connection
